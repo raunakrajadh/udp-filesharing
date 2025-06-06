5ec97ebee090c0b0a619e2f5c69636e7dd071bb2
unknown <raunakrajadh@gmail.com>
2025-06-06 14:30:23 +0545

    update

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const PORT = 8005;
 // Middleware
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'uploads')));
 app.use(express.urlencoded({ extended: true }));
 
